d4ab4e09a490479be57a0b8ca4f3bac8f9832785
ivanv257 <ivanv257@gmail.com>
2018-02-05 23:02:41 -0500

    Script - Testing div CSS grid container

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ const body = document.querySelector('body');
 
 const div = createElement('div');
 div.classList.add('gridContainer');
+div.textContent = 'Hello this is the gridContainer and if this text appears the grid container should be active.'
+
 body.appendChild(div);
