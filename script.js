8de3a3df534986ad55d17e01f937d1fd8bdc01b3
ivanv257 <ivanv257@gmail.com>
2018-02-05 23:29:10 -0500

    Script - style added to divChild

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const divContainer = document.querySelector('#divContainer');
 
 const divChild = document.createElement('div');
 divChild.classList.add('gridChild');
+
+divChild.setAttribute('style', 'color: blue');
 divChild.textContent = 'Hello this is the gridChild and if this text appears the gridChild should be active.'
+
 
 divContainer.appendChild(divChild);
