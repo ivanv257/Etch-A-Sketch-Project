fd86c1398f5229eb3ff9486e22f599d76a56ec47
ivanv257 <ivanv257@gmail.com>
2018-02-05 22:57:45 -0500

    Script  - Create and Append the Main Grid Container

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+// Create and Append the Main Grid Container
+const body = document.querySelector('body');
+
+const div = createElement('div');
+div.classList.add('gridContainer');
+body.appendChild(div);
