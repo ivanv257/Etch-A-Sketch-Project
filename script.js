df56fdad13eec0fd090bc6e17b4b6ebca6d43ef8
ivanv257 <ivanv257@gmail.com>
2018-02-06 10:35:57 -0500

    Script - test runs and debugging

--- script.js
+++ script.js
@@ -8,5 +8,12 @@ divChild.classList.add('gridChild');
 // Check the grid container border. 
 gridContainer.setAttribute('style', 'border: 1px solid black; height: 1200px; width: 1200px');
 
-
 gridContainer.appendChild(divChild);
+
+//Test function
+function addContainerChild (){
+  if (5 < 10){
+  return divChild.setAttribute('style', 'border: 1px solid black; height: 600px; width: 600px');
+  }
+}
+
