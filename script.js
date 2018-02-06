e9a4f25b9d6f8d50d683df66878b24de2621860c
ivanv257 <ivanv257@gmail.com>
2018-02-05 23:27:05 -0500

    Script - debugged test on div containers

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Create and Append the Main Grid Container
 const divContainer = document.querySelector('#divContainer');
 
-const divChild = createElement('div');
+const divChild = document.createElement('div');
 divChild.classList.add('gridChild');
 divChild.textContent = 'Hello this is the gridChild and if this text appears the gridChild should be active.'
 
