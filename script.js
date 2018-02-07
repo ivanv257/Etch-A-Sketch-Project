677b1a5ad5a97bf72ec73d40085e7caa39ef8823
ivanv257 <ivanv257@gmail.com>
2018-02-06 22:53:04 -0500

    8 Grid items apended - test to see effect

--- script.js
+++ script.js
@@ -4,23 +4,15 @@ const gridContainer = document.querySelector('.grid-container');
 gridContainer.setAttribute('style', 'display: grid; grid-template-columns: auto auto auto; border: 1px solid black; height: 600px; width: 600px');
 
 // Grid items
-const gridItem1 = document.createElement('div');
-gridItem1.classList.add('grid-item1');
-gridContainer.appendChild(gridItem1);
-gridItem1.setAttribute('style', 'border: 1px solid black; background-color: black')
+const gridItem = document.createElement('div');
+gridItem.classList.add('grid-item');
+gridContainer.appendChild(gridItem);
+gridItem.setAttribute('style', 'border: 1px solid black')
 
-const gridItem2 = document.createElement('div');
-gridItem2.classList.add('grid-item2');
-gridContainer.appendChild(gridItem2);
-gridItem2.setAttribute('style', 'border: 1px solid black; background-color: yellow')
-
-const gridItem3 = document.createElement('div');
-gridItem3.classList.add('grid-item3');
-gridContainer.appendChild(gridItem3);
-gridItem3.setAttribute('style', 'border: 1px solid black; background-color: blue')
-
-const gridItem4 = document.createElement('div');
-gridItem4.classList.add('grid-item4');
-gridContainer.appendChild(gridItem4);
-gridItem4.setAttribute('style', 'border: 1px solid black; background-color: blue')
-
+gridContainer.appendChild(gridItem);
+gridContainer.appendChild(gridItem);
+gridContainer.appendChild(gridItem);
+gridContainer.appendChild(gridItem);
+gridContainer.appendChild(gridItem);
+gridContainer.appendChild(gridItem);
+gridContainer.appendChild(gridItem);
