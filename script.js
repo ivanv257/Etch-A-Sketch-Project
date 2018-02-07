09d5e4dbf51023784911933d16891bd96a74556d
ivanv257 <ivanv257@gmail.com>
2018-02-06 22:39:26 -0500

    first grid item added to css grid with Script

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 
-// Create and Append the Main Grid Container
+// Select and style the main Grid container
 const gridContainer = document.querySelector('.grid-container');
+gridContainer.setAttribute('style', 'display: grid; grid-template-columns: auto auto auto; border: 1px solid black; height: 600px; width: 600px');
 
-const divChild = document.createElement('div');
+// The first grid item
+const gridItem1 = document.createElement('div');
 gridItem1.classList.add('grid-item1');
-
-// Check the grid container border. 
-gridContainer.setAttribute('style', 'border: 1px solid black; height: 600px; width: 600px');
-
 gridContainer.appendChild(gridItem1);
-
+gridItem1.setAttribute('style', 'border: 1px solid black; background-color: black')
 
