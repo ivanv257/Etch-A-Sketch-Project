d1567e737932c545fdfc3d8de575ce8d74b465c4
ivanv257 <ivanv257@gmail.com>
2018-02-06 23:30:35 -0500

    center grid container

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // Select and style the main Grid container
 const gridContainer = document.querySelector('.grid-container');
-gridContainer.setAttribute('style', 'display: grid; grid-template-columns: auto auto auto; border: 1px solid black; height: 600px; width: 600px');
+gridContainer.setAttribute('style', 'display: grid; grid-template-columns: auto auto auto; border: 1px solid black; height: 600px; width: 600px; text-align: center;');
 
 // Grid items
 const gridItem = document.createElement('div');
