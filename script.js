4b1baf0734d2425f9ef280ad8e6c371692488780
ivanv257 <ivanv257@gmail.com>
2018-02-19 22:56:37 -0500

    Script Refactor the script

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 
-// Select and style the main Grid container
-const gridContainer = document.querySelector('.grid-container');
-gridContainer.setAttribute('style', 'display: grid; grid-template-columns: auto auto auto; border: 1px solid black; height: 600px; width: 600px; margin-left: 25%;');
+let body = document.querySelector('body');
+const container = document.querySelector('#container');
 
-
-// Grid items
-const gridItem = document.createElement('div');
-gridItem.classList.add('grid-item');
-gridContainer.appendChild(gridItem);
-gridItem.setAttribute('style', 'border: 1px solid black');
-
-
-gridContainer.appendChild(gridItem);
-
-
-
-
-gridItem.addEventListener('mouseover', hoverFunction);
-
-function hoverFunction(){
-  gridItem.setAttribute('style', 'background-color: black')  
-}
 
