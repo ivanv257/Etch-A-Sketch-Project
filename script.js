189f107ef1133036c5034139c96b6e4ae85900f3
ivanv257 <ivanv257@gmail.com>
2018-02-19 23:21:24 -0500

    Working in Progress Script

--- script.js
+++ script.js
@@ -2,4 +2,22 @@
 let body = document.querySelector('body');
 const container = document.querySelector('#container');
 
+let squareSubmit = document.querySelector('.squareSubmit');
+let squareNumber = document.querySelector('.squareNumber');
 
+let gridSize = 16;
+let isRainbowActive = false;
+
+let counter = 1;
+//Colors to be selected.
+let colorArray = [''];
+
+//Create the grid - gridSize length * gridSize Height
+function createGrid(){ 
+  for (var i=o; i < gridSize*gridSize; i++){
+    let unit = document.createElement('div');
+    unit.className= 'unit';
+    container.appendChild(unit);
+    unit.addEventListener()
+  }
+}
