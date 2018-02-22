431daf9447f15ee9ce90a69eb0403789aca02a30
ivanv257 <ivanv257@gmail.com>
2018-02-21 23:36:47 -0500

    Syntax error corrected

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let gridSize = 16;
 
 //Create the grid - gridSize length * gridSize Height
 function createGrid(){ 
-  for (var i=o; i < gridSize*gridSize; i++){
+  for (var i=0; i < gridSize*gridSize; i++){
     let unit = document.createElement('div');
     unit.className= 'unit';
     container.appendChild(unit);
