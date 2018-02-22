93286870e1422d964d462114bfba650e375b0225
ivanv257 <ivanv257@gmail.com>
2018-02-21 22:59:41 -0500

    Script - Basic 'for' loop create for divs

--- script.js
+++ script.js
@@ -2,15 +2,7 @@
 let body = document.querySelector('body');
 const container = document.querySelector('#container');
 
-let squareSubmit = document.querySelector('.squareSubmit');
-let squareNumber = document.querySelector('.squareNumber');
-
 let gridSize = 16;
-let isRainbowActive = false;
-
-let counter = 1;
-//Colors to be selected.
-let colorArray = [''];
 
 //Create the grid - gridSize length * gridSize Height
 function createGrid(){ 
@@ -18,6 +10,5 @@ function createGrid(){
     let unit = document.createElement('div');
     unit.className= 'unit';
     container.appendChild(unit);
-    unit.addEventListener()
   }
 }
