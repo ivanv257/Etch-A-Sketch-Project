0afdcf65f2ba0fa23a0186620886309b11e3ce45
ivanv257 <ivanv257@gmail.com>
2018-02-22 23:02:16 -0500

    Script refactored although not entirely original WIP

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-
 let body = document.querySelector('body');
 const container = document.querySelector('#container');
 
 let gridSize = 16;
 
-//Create the grid - gridSize length * gridSize Height
-function createGrid(){ 
-  for (var i=0; i < gridSize*gridSize; i++){
+//Create the grid - gridSize length * gridSize Height\
+function createGrid() {
+  for (var i = 0; i < gridSize * gridSize; i++) {
     let unit = document.createElement('div');
-    unit.className= 'unit';
+    unit.className = 'unit';
     container.appendChild(unit);
+    unit.setAttribute('style', 'border: 1px solid black');
   }
 }
 
+function generateString() {
+  var columns = "";
+  var rows = "";
+  for (var i = 0; i < gridSize; i++) {
+    rows += "1fr "
+    columns += " 1fr"
+  }
+  rows += "/ ";
+  rows = rows.concat(columns);
+  return rows;
+}
+
+document.querySelector('#container').style.grid = generateString();
 createGrid();
